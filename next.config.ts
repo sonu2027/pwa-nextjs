c5e686e799ca114c60f29f4ba4d3609d25df593a
Sonu Mondal <takeinfo2u@gmail.com>
2025-03-17 01:05:46 +0530

    exculded manifest.json

--- next.config.ts
+++ next.config.ts
@@ -7,11 +7,29 @@
 
 // export default nextConfig;
 
+// const withPWA = require('next-pwa')({
+//   dest: 'public', // Destination directory for the service worker
+//   disable: process.env.NODE_ENV === 'development', // Disable PWA in development
+//   register: true, // Automatically register the service worker
+//   skipWaiting: true, // Skip waiting for the service worker to activate
+// });
+
+// module.exports = withPWA({
+//   reactStrictMode: true,
+// });
+
+
+
+
 const withPWA = require('next-pwa')({
-  dest: 'public', // Destination directory for the service worker
-  disable: process.env.NODE_ENV === 'development', // Disable PWA in development
-  register: true, // Automatically register the service worker
-  skipWaiting: true, // Skip waiting for the service worker to activate
+  dest: 'public',
+  disable: process.env.NODE_ENV === 'development',
+  register: true,
+  skipWaiting: true,
+  // Exclude specific files from precaching
+  exclude: [
+    /_next\/app-build-manifest\.json$/, // Exclude app-build-manifest.json
+  ],
 });
 
 module.exports = withPWA({
