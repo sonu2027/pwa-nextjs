145223ec411587626a97e0514a8b28810db5c1b5
Sonu Mondal <takeinfo2u@gmail.com>
2025-03-17 10:20:53 +0530

    create design

--- next.config.ts
+++ next.config.ts
@@ -7,18 +7,6 @@
 
 // export default nextConfig;
 
-// const withPWA = require('next-pwa')({
-//   dest: 'public', // Destination directory for the service worker
-//   disable: process.env.NODE_ENV === 'development', // Disable PWA in development
-//   register: true, // Automatically register the service worker
-//   skipWaiting: true, // Skip waiting for the service worker to activate
-// });
-
-// module.exports = withPWA({
-//   reactStrictMode: true,
-// });
-
-
 
 
 const withPWA = require('next-pwa')({
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -27,35 +27,53 @@ export default function Home() {
     // Display the notification
     new Notification('Hello!', {
       body: 'You received a push notification.',
-      icon: '/img.jpeg', 
+      icon: '/img.jpeg',
     });
   };
 
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 text-center">
-      <h1 className="text-4xl font-bold mb-8">Welcome to Our App</h1>
+    <div className="flex flex-col items-center justify-between min-h-screen bg-gradient-to-b from-[#311b7f] to-[#030e1a] text-center">
+      <div className='mt-5'>
+        <h1 className="text-xl sm:text-4xl font-bold mb-8">Welcome to Our App</h1>
+        <div className='w-72 h-72 sm:w-80 sm:h-80 rounded-full border-solid border-12  border-[#332176] flex justify-center items-center'>
+          <div className='w-56 h-56 sm:w-64 sm:h-64 rounded-full border-solid border-2  border-[#3b2883] flex justify-center items-center'>
+            <div className='w-48 h-48 sm:w-56 sm:h-56 rounded-full border-solid border-2  border-gray-500 flex justify-center items-center'>
+              <div onClick={requestNotificationPermission} className='w-40 h-40 sm:h-44 sm:w-44 rounded-full bg-[#432486] flex justify-center items-center'>
+                <img src="/Vector.png" alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+        {notificationPermission === 'denied' && (
+          <p className="text-red-500 mb-4">
+            Notifications are blocked. Please enable them in your browser settings.
+          </p>
+        )}
+      </div>
+      <div className='mb-12 flex flex-col gap-y-4 justify-between items-center'>
+
+        <div><h1 className="text-xl font-bold my-2">Lorem ipsum dolor</h1>
+          <p className='text-gray-300'>Lorem ipsum dolor sit amet consectetur .</p></div>
+
+        <button
+          onClick={sendNotification}
+          disabled={notificationPermission !== 'granted'}
+          className={`${notificationPermission === 'granted'
+            ? 'bg-green-500 border-4 border-solid border-[#30916c] hover:bg-green-600'
+            : 'bg-[#432486] border-4 border-solid border-[#5b34b1] cursor-not-allowed'
+            } text-white px-20 py-2 rounded-lg shadow-md transition-colors`}
+        >
+          Send Notification
+        </button>
+      </div>
+      {/* 
       <button
         onClick={requestNotificationPermission}
         className="bg-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:bg-blue-600 transition-colors mb-4"
       >
         Enable Notifications
       </button>
-      {notificationPermission === 'denied' && (
-        <p className="text-red-500 mb-4">
-          Notifications are blocked. Please enable them in your browser settings.
-        </p>
-      )}
-      <button
-        onClick={sendNotification}
-        disabled={notificationPermission !== 'granted'}
-        className={`${
-          notificationPermission === 'granted'
-            ? 'bg-green-500 hover:bg-green-600'
-            : 'bg-gray-400 cursor-not-allowed'
-        } text-white px-6 py-2 rounded-lg shadow-md transition-colors`}
-      >
-        Send Notification
-      </button>
+       */}
     </div>
   );
 }
